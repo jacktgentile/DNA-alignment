f9b6b1f462f3919de2f5acedb71aad9a1ee80cdb
jacktgentile <32143117+jacktgentile@users.noreply.github.com>
2019-03-04 20:49:37 -0600

    file input/output functional

---
+++ optimal_align.py
@@ -0,0 +1,40 @@
+import sys
+
+# return string of base pairs
+def load_sequence(filename):
+    f = open(filename)
+    read_str = f.read()
+    read_list = read_str.split("\n")
+    result = ""
+    for substr in read_list:
+        if len(substr) == 0:
+            continue
+        if substr[0] != '>' and substr[0] != ';':
+            result += substr
+    return result
+
+# return 2D list with substitution matrix values
+def load_submatrix(filename):
+    f = open(filename)
+    read_str = f.read()
+    read_list = read_str.split()
+    result = []
+    for i in range(4):
+        templist = []
+        for j in range(4):
+            templist.append(int(read_list[4*i+j]))
+        result.append(templist)
+    return result
+
+
+# main function
+if __name__ == "__main__":
+    # take in parameters
+    fastafilename1 = sys.argv[1]
+    fastafilename2 = sys.argv[2]
+    submatrixfile = sys.argv[3]
+    gapPenalty = sys.argv[4]
+    # assign values from file contents
+    sequence1 = load_sequence(fastafilename1)
+    sequence2 = load_sequence(fastafilename2)
+    submatrix = load_submatrix(submatrixfile)
