f1daf5b8a5eccce0e244c97c665ee8e8d3386bc1
jacktgentile <32143117+jacktgentile@users.noreply.github.com>
2019-03-06 09:39:15 -0600

    create and align random mutations of a sequence

---
+++ create_data_and_align.py
@@ -0,0 +1,59 @@
+import sys
+from random import randint, seed
+from optimal_align import load_submatrix, align
+
+# generate random bp sequence of length L
+def random_sequence(L):
+    baselist = ["A","C","G","T"]
+    result = []
+    for i in range(L):
+        result.append(baselist[randint(0,3)])
+    return result
+
+# generate mutation of given sequence based on project description
+def mutate_sequence(sequence):
+    new_sequence = sequence.copy()
+    L = len(sequence)
+    baselist = ["A","C","G","T"]
+    # make L/10 random changes
+    for i in range(int(L/10)):
+        idx = randint(0,len(new_sequence)-1)
+        if randint(0,1) == 0:
+            # deletion
+            new_sequence.pop(idx)
+        else:
+            # change base
+            offset = randint(1,3)
+            j = baselist.index(new_sequence[idx])
+            j = (j + offset) % 4
+            new_sequence[idx] = baselist[j]
+    return new_sequence
+
+# write the sequence to a fasta file
+def write_sequence_to_file(sequence, filename, seq_desc="random gene sequence"):
+    # opens filename in write-only mode, erases original file content
+    f = open(filename, 'w')
+    f.write(">" + seq_desc + "\n")
+    count = 0
+    for bp in sequence:
+        f.seek(0,2)
+        f.write(bp)
+        count += 1
+        if (count % 80) == 0:
+            count = 0
+            f.seek(0,2)
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    seed()
+    # take L parameter
+    L = int(sys.argv[1])
+    submatrix = load_submatrix("subs.txt")
+    origin_seq = random_sequence(L)
+    seq2 = mutate_sequence(origin_seq)
+    seq3 = mutate_sequence(origin_seq)
+    write_sequence_to_file(origin_seq, "seq1.fasta", "unmutated sequence")
+    write_sequence_to_file(seq2, "seq2.fasta", "mutated sequence 1")
+    write_sequence_to_file(seq3, "seq3.fasta", "mutated sequence 2")
+    align(seq2, seq3, submatrix, -5)
--- optimal_align.py
+++ optimal_align.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 # return string of base pairs
 def load_sequence(filename):
@@ -26,9 +27,88 @@ def load_submatrix(filename):
         result.append(templist)
     return result
 
-# calculate optimal alignment of 2 sequences
+# calculate optimal alignment of 2 sequences and store results in .txt file
 def align(sequence1, sequence2, submatrix, gapPenalty):
-    return 0
+    baselist = ["A","T","C","G"]
+    width = len(sequence1)
+    height = len(sequence2)
+    alignment_scores = np.zeros((height+1, width+1), dtype=int)
+    alignment_prev = np.zeros((height, width), dtype=int) # TODO describe behavior
+    # determine score matrix
+    for i in range(1, height+1):
+        for j in range(1, width+1):
+            n = baselist.index(sequence1[j-1])
+            m = baselist.index(sequence2[i-1])
+            # score if match/mismatch
+            score1 = submatrix[n][m] + alignment_scores[i-1][j-1]
+            # score if down movement
+            score2 = gapPenalty + alignment_scores[i-1][j]
+            # score if right movement
+            score3 = gapPenalty + alignment_scores[i][j-1]
+            if score1 >= score2 and score1 >= score2 and score1 >= 0:
+                alignment_scores[i][j] = score1
+                alignment_prev[i-1][j-1] = 1
+            elif score2 >= score1 and score2 >= score3 and score2 > 0:
+                alignment_scores[i][j] = score2
+                alignment_prev[i-1][j-1] = 2
+            elif score3 >= score1 and score3 >= score2 and score3 > 0:
+                alignment_scores[i][j] = score3
+                alignment_prev[i-1][j-1] = 3
+            else:
+                alignment_scores[i][j] = 0
+                alignment_prev[i-1][j-1] = 1
+    # find maximum scoring path
+    max_score = 0
+    coord = (-1,-1)
+    for i in range(height):
+        for j in range(width):
+            if alignment_scores[i+1][j+1] >= max_score:
+                max_score = alignment_scores[i+1][j+1]
+                coord = (i,j)
+    # modified sequences
+    modseq1 = []
+    modseq2 = []
+    curx = coord[1]
+    cury = coord[0]
+    # end behavior
+    if curx < width-1:
+        for i in range(width-1, curx, -1):
+            modseq1.append(sequence1[i])
+            modseq2.append("-")
+    elif cury < height-1:
+        for i in range(cury+1, height):
+            modseq1.append("-")
+            modseq2.append(sequence2[i])
+    # backtracking
+    while curx >= 0 and cury >= 0:
+        dir = alignment_prev[cury][curx]
+        if dir == 1:
+            modseq1.append(sequence1[curx])
+            modseq2.append(sequence2[cury])
+            curx -= 1
+            cury -= 1
+        elif dir == 2:
+            modseq1.append("-")
+            modseq2.append(sequence2[cury])
+            cury -= 1
+        else:
+            modseq1.append(sequence1[curx])
+            modseq2.append("-")
+            curx -= 1
+    # end behavior
+    if curx >= 0:
+        for i in range(curx, -1, -1):
+            modseq1.append(sequence1[i])
+            modseq2.append("-")
+    elif cury >= 0:
+        for i in range(cury, -1, -1):
+            modseq1.append("-")
+            modseq2.append(sequence2[i])
+    # print("".join(modseq1)[::-1])
+    # print("".join(modseq2)[::-1])
+    f = open("results.txt",'w')
+    f.write("The optimal alignment between given sequences has score " + str(max_score) + ".\n")
+    f.write("".join(modseq1)[::-1] + "\n" + "".join(modseq2)[::-1])
 
 # main function
 if __name__ == "__main__":
@@ -41,3 +121,6 @@ if __name__ == "__main__":
     sequence1 = load_sequence(fastafilename1)
     sequence2 = load_sequence(fastafilename2)
     submatrix = load_submatrix(submatrixfile)
+    # print(sequence1)
+    # print(sequence2)
+    align(sequence1, sequence2, submatrix, gapPenalty)
